d6fb44d833c48a1207e057448ec6df2ca6c644c2
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-10-08 15:38:00 -0500

    started work on game_floor class, set up a few initial methods for us to work on.

---
+++ game_floor.py
@@ -0,0 +1,19 @@
+from players import Players
+from human import Human
+from ai import AI
+
+class game:
+    def __init__(self):
+        pass
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def single_player(self):
+        pass
+
+    def multi_player(self):
+        pass
