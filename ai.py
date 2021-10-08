f6362426b607a16c7ffe702b08bba7f214045329
LatoyaNixon <nixonlatoya.ln@gmail.com>
2021-10-08 16:05:59 -0400

    completed the code for the AI class

---
+++ ai.py
@@ -0,0 +1,22 @@
+from player import Player 
+class AI(Player):
+    def __init__(self):
+        self.choose_gesture = ''
+        super().__init__()
+        self.set_name()
+
+    def gesture(self):
+        self.select_gesture = input('/n Please enter: rock, paper, scissors, lizard, spock\n')
+        if self.gesture_select  in  self.ai_gestures:
+            print(self.choice)
+        else:
+            while self.gesture_select not in self.ai_gestures:
+                print("/n not a choice.. Please select: rock, papers, scissors, lizard, sock/n")
+                self.choice = input("/nPlease enter: rock, paper, scissors, lizard, spock\n" )
+
+        print (f'gesure choosen: {self.gesture_select}')
+
+
+    def set_name(self):
+        self.name = input("/n Please select player name/n")
+        print(self.name)
